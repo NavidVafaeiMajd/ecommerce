21ceafe0a21206ea5a1eed558b435637d0d09e36
Navid <vafaermn@gmail.com>
2025-11-20 11:36:44 +0330

    dark mode and light mode

--- components/sections/MobileMenu.tsx
+++ components/sections/MobileMenu.tsx
@@ -1,4 +1,6 @@
 import { ReactNode, useState } from "react";
+import { Input } from "../ui/input";
+import { HeaderBtn } from "./headerBtn";
 interface Props {
   item: ReactNode;
 }
@@ -13,16 +15,23 @@ const MobileMenu = ({ item }: Props) => {
         </span>
       </div>
       <div
-        className={` fixed top-0! h-screen! -right-1000 w-full bg-black/60 ${isOpen ? "right-0" : ""}`}
+        className={` fixed top-0! h-screen! -right-1000 w-full bg-background/10 backdrop-blur ${isOpen ? "right-0" : ""}`}
         id="mobile-menu"
       >
         <div className="grid grid-cols-2!">
-          <div className="bg-ackground!">
-            <div>
-              <ul>{item}</ul>
+          <div className="bg-background! p-5 flex flex-col justify-between h-screen! shadow">
+            <div className="flex flex-col gap-5">
+              <Input
+                variant={"filled"}
+                placeholder="Search"
+                type="search"
+                className="min-w-full! "
+              />
+              <ul className="flex flex-col items-center gap-5">{item}</ul>
             </div>
+            <HeaderBtn />
           </div>
-          <div>
+          <div className="text-right text-2xl text-bold! p-5">
             <span onClick={() => setIsOpen(!isOpen)}>close</span>
           </div>
         </div>
