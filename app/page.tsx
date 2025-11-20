64e7ec51104f7b79e37867658846af0330ffbd50
Navid <vafaermn@gmail.com>
2025-11-20 17:50:10 +0330

    secound sec

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import SecoundSec from "@/components/sections/home/SecoundSec";
 import { Slider } from "@/components/sections/home/slider";
 
 export default function Home() {
   return (
-    <div className="">
-      <Slider/>
+    <div className="flex flex-col gap-10">
+      <Slider />
+      <SecoundSec />
     </div>
   );
 }
