ae5a1db4824a7bdf174dbb5d4017b7c4b75ad507
Navid <vafaermn@gmail.com>
2025-11-24 20:03:02 +0330

    compilite wishlist

--- app/MyAccount/Wishlist.tsx
+++ app/MyAccount/Wishlist.tsx
@@ -1,24 +1,53 @@
+import { Button } from "@/components/ui/button";
+import CliseIcon from "@/public/icons/colse";
+import Image from "next/image";
+
 const Wishlist = () => {
   return (
     <>
       <div className="text-foreground! mb-5">
-        <h3 className="special mb-2!"> Whishlist</h3>
+        <h3 className="special mb-2!"> Wishlist</h3>
       </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-5">
-              {[...Array(5)].map((_, index) => (
-                <WishlistItem key={index} />
-              ))}Wishlist
-            </div>
+      <div className="flex flex-col gap-5">
+        {[...Array(5)].map((_, index) => (
+          <div
+            key={index}
+            className="border-b border-gray-300 last:border-none pb-4"
+          >
+            <WishlistItem />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
 
 export default Wishlist;
 
-function  WishlistItem()  {
-    return (
-        <div>
-            
-    </div>);
+function WishlistItem() {
+  return (
+    <div className="flex gap-5 items-center justify-between!">
+      <div>
+        <CliseIcon />
+      </div>
+      <div className="relative w-25! h-25! rounded-2xl overflow-hidden">
+        <Image alt="" src={"/home/test2.svg"} fill className="object-cover! " />
+      </div>
+      <div className="flex flex-col">
+        <h4>Blue Flower Print Crop Top</h4>
+        <span>
+          Color : <span>Yellow</span>
+        </span>
+        <span>
+          Quantity : <span>1</span>
+        </span>
+      </div>
+      <div>
+        <span className="text-ring">$20.00</span>
+      </div>
+      <div>
+        <Button>Add To Cart</Button>
+      </div>
+    </div>
+  );
 }
- 
--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -26,7 +26,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex w-fit items-center justify-center rounded-lg p-[3px]",
+        "bg-muted text-muted-foreground w-fit items-start justify-start rounded-lg p-[3px]",
         className
       )}
       {...props}
@@ -42,7 +42,7 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        " focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        " focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props}
