9e4773aeb7b925512d3de477d8d5d12501451ecd
Navid <vafaermn@gmail.com>
2025-11-27 21:17:05 +0330

    install radio and add to product section

---
+++ components/sections/shop/SizeRadioGroup.tsx
@@ -0,0 +1,33 @@
+import { RadioGroup, RadioGroupItem } from "../../ui/radio-group";
+import { Label } from "../../ui/label";
+import { cn } from "@/lib/utils";
+
+const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+
+export function SizeRadioGroup() {
+  return (
+    <RadioGroup
+      defaultValue="M"
+      className="flex flex-wrap items-center gap-3 my-3"
+    >
+      {sizes.map((size, index) => (
+        <div key={size}>
+          <Label
+            htmlFor={`size-${index}`}
+            className={cn(
+              "flex items-center justify-center w-10 h-10 rounded-xl cursor-pointer border transition",
+              "has-[[data-state=checked]]:bg-foreground has-[[data-state=checked]]:text-background"
+            )}
+          >
+            <RadioGroupItem
+              id={`size-${index}`}
+              value={size}
+              className="peer sr-only"
+            />
+            {size}
+          </Label>
+        </div>
+      ))}
+    </RadioGroup>
+  );
+}
