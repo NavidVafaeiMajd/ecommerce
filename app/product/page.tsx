1c456f187b0498f64380dcec761f36e1c8fd03c7
Navid <vafaermn@gmail.com>
2025-11-29 12:17:22 +0330

    fix and text build

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -63,7 +63,8 @@ const page = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mt-5">
           {[...Array(8)].map((_, index) => (
-            <ProductItem key={index} />
+            // <ProductItem key={index} />
+            <></>
           ))}
         </div>
       </div>
--- components/sections/home/LimeLight.tsx
+++ components/sections/home/LimeLight.tsx
@@ -5,10 +5,10 @@ const LimeLight = () => {
     <div>
       <h3 className="special">In The LimeLight</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
+        {/* <ProductItem />
         <ProductItem />
         <ProductItem />
-        <ProductItem />
-        <ProductItem />
+        <ProductItem /> */}
       </div>
     </div>
   );
