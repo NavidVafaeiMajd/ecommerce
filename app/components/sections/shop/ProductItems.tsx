7dd622a67ff02af3bd4e635872326d09ec9ecdfe
Navid <vafaermn@gmail.com>
2025-12-06 16:56:25 +0330

    use debounce for price changes

--- app/components/sections/shop/ProductItems.tsx
+++ app/components/sections/shop/ProductItems.tsx
@@ -2,17 +2,17 @@
 import { ProductItem } from "./ProductItem";
 import { ProductListItem } from "@/app/lib/definitions";
 
-export function  ProductItems({products , cat} : {products? : ProductListItem[], cat?: string}) {
+export function ProductItems({ products, cat }: { products?: ProductListItem[], cat?: string }) {
+  console.log(products)
   return (
     <>
     <div>
       <div>
-        <h3 className="text-2xl">{cat || "Shop"} </h3>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-5">
-        { products?.map((product , index) => (
+      { products?.length!==0 ? products?.map((product , index) => (
           <ProductItem key={index} product={ product } />
-        ))}
+        )) :<><h3 className="text-center">NOT FOUND ENYTHING</h3></>}
       </div>
     </div></>
   );
