9bf6945f42c809f87e56d1cf8509cf95102d73fb
Navid <vafaermn@gmail.com>
2025-12-07 09:57:38 +0330

    base of authentication

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import { Pool } from "pg";
+import { betterAuth } from "better-auth";
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+})
+
+export const auth = betterAuth({
+  database: pool,
+  emailAndPassword: {
+    enabled: true,
+  },
+});
