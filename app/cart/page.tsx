5b728265296afd8064a80af050d56bdfc8450421
Navid <vafaermn@gmail.com>
2025-12-08 23:43:00 +0330

    total price in cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -33,7 +33,12 @@ async function getData(): Promise<Payment[]> {
 
 const Page =  () => {
       const { items } = useCart();
-  
+    const subTotal = items.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
+  );
+  const shipping = items.length > 0 ? 5 : 0; 
+  const grandTotal = subTotal + shipping;
 
   return (
     <div className="">
@@ -42,15 +47,15 @@ const Page =  () => {
         <div className="md:w-1/2 bg-grayColor! p-5 mt-5">
           <div className="flex justify-between">
             <span>Sub Total</span>
-            <span>$532.00</span>
+            <span>${ subTotal}.00</span>
           </div>
           <div className="flex justify-between">
             <span>Shipping </span>
-            <span>$5.00</span>
+            <span>${shipping}.00</span>
           </div>
           <div className="flex justify-between font-bold mt-5 border-b-1 pb-5">
             <span>Grand Total</span>
-            <span>$537.00</span>
+            <span>${grandTotal}.00</span>
           </div>
           <div className="flex justify-center p-5">
             <Button>Proceed To Checkout</Button>
